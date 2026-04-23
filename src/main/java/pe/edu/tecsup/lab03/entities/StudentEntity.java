67895ceea01398d5a3028d2268fac3ac7624c8e6
haroldsantivanez-ship-it <harold.santivanez@tecsup.edu.pe>
2026-04-23 06:32:49 -0500

    adicion de StudentEntity, y funcion para imprimir mi nombre

---
+++ src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
@@ -0,0 +1,10 @@
+package pe.edu.tecsup.lab03.entities;
+
+public class StudentEntity {
+    String a = "Harold Santivañez";
+
+    public static void main(String[] args) {
+        StudentEntity obj = new StudentEntity();
+        System.out.println(obj.a);
+    }
+}
